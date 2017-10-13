cd3ea2cfa1aa48e55d35faa50570ec303ee6eea0
Manuel Moritz-Schliesing <manuel@bitmade.de>
2017-10-13 11:20:01 +0200

    Remove dotenv package

--- scripts/server.js
+++ scripts/server.js
@@ -6,11 +6,6 @@
 
 process.env.NODE_ENV = 'development';
 
-// Load environment variables from .env file. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.
-require('dotenv').config({ silent: true });
-
 const chalk = require('chalk');
 const argv = require('minimist')(process.argv.slice(2));
 const path = require('path');
